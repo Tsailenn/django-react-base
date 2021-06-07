5b829f05c25faea2abef518cd48dd90413efefb7
Tsailenn <reaparus@gmail.com>
2021-06-07 19:50:59 +0700

    did an oopsie

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w+4dg2n261l^3rmr3n(7e7k9q71rjy1rnv2$t#*arr)_d^m@p9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
